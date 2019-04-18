43151eb848c6c595a60717f48f0120217a822e8e
z2486357 <z2486357@gmail.com>
2019-04-18 16:08:05 -0400

    data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  styles:[`
-    h3 {
-      color: dodgerblue
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  serverElements = [{type:'server',name:'Testserver',content:'Just a test!'}];
+
+  onServerAdd(serverData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdd(blueprintData:{blueprintName:string,blueprintContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.blueprintName,
+      content: blueprintData.blueprintContent
+    });
+  }
+  
+
+ 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { FormsModule } from '@angular/forms';
-
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-
-}
+export class AppModule { }
