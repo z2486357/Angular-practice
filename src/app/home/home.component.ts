d535b3134165f30999f47cf859799d74b05402cf
z2486357 <z2486357@gmail.com>
2019-04-21 11:37:34 -0400

    update observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor(private router:Router, private authservice:AuthService) { }
+export class HomeComponent implements OnInit,OnDestroy {
+  numberobssubscription:Subscription
+  customobssubscription:Subscription
+  constructor() { }
 
   ngOnInit() {
+    const myNumbers=Observable.interval(500)
+      .map(
+        (data:number)=>{
+          return data*2
+        }
+      )
+    this.numberobssubscription=myNumbers.subscribe(
+      (number:number)=>{
+        console.log(number)
+      }
+    )
+
+    const myObservable=Observable.create(
+      (observer:Observer<string>)=>{
+        setTimeout(()=>{
+            observer.next('first package');
+          },2000);
+        setTimeout(()=>{
+            observer.next('second package');
+          },4000);
+        // setTimeout(()=>{
+        //     observer.error('this does not work');
+        //   },5000);
+        setTimeout(()=>{
+            observer.complete();
+          },6000);
+        setTimeout(()=>{
+            observer.next('third package');
+          },7000);
+      }
+    );
+
+    this.customobssubscription=myObservable.subscribe(
+      (data:string)=>{console.log(data)},
+      (error:string)=>{console.log(error)},
+      ()=>{console.log('complete')},
+    )
   }
 
-  onLoadServers(id:number){
-    this.router.navigate(['/servers',id,'edit'],{queryParams:{allowEdit:1},fragment:'loading'})
+  ngOnDestroy(){
+    this.customobssubscription.unsubscribe()
+    this.numberobssubscription.unsubscribe()
   }
-  onLogin(){
-    this.authservice.login()
-  }
-  onLogout(){
-    this.authservice.logout()
-  }
+
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  id: number;
+
+  constructor(private route: ActivatedRoute, private userservice:UserService) { }
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+        }
+      );
+  }
+  onActivate(){
+    this.userservice.userActivated.next(this.id)
+  }
+
+}
