16d9e7e5f5ce31a6bf0f8c0484b207a3a988f71b
z2486357 <z2486357@gmail.com>
2019-05-06 23:15:57 -0400

    form react finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -15,15 +16,35 @@ export class AppComponent implements OnInit{
     this.signupForm = new FormGroup({
       userData: new FormGroup({
         username: new FormControl(null,[Validators.required,this.forbiddenNames.bind(this)]),
-        email: new FormControl('xxx@gmail.com',[Validators.required,Validators.email]),
+        email: new FormControl('xxx@gmail.com',[Validators.required,Validators.email],this.forbiddenEmails),
       }),
       gender: new FormControl('male'),
       hobbies:new FormArray([])
+    });
+
+    //this.signupForm.valueChanges.subscribe(
+    //(value)=>console.log(value))
+    this.signupForm.statusChanges.subscribe(
+      (value) => console.log(value))
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Aloha',
+        'email': 'haha@gmail.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'I change my name'
+      }
     });
   }
 
   submit() {
     console.log(this.signupForm)
+    this.signupForm.reset({
+    'gender':'male'})
   }
 
   AddHobby() {
@@ -36,5 +57,17 @@ export class AppComponent implements OnInit{
       return {'nameIsForbidden':true}
     } 
     return null;
+  }
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ 'emailIsForbidden': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
